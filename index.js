57359c44ce88cc924abb90cacede836573fef386
neald44 <neald44@gmail.com>
2024-06-22 12:31:49 -0400

    CSS fixes and Javascrip scroll to top

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// For Accordian
 const accordian = document.getElementsByClassName('accordian');
 var i;
 
@@ -12,3 +13,26 @@ for (i = 0; i < accordian.length; i++) {
         }
     });
 }
+
+// For Copyright Year
+
+var currentYear = new Date().getFullYear();
+document.getElementById('copyYear').innerText = currentYear;
+
+// For Scroll To Top
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        document.getElementById("scrollToTop").style.display = "block";
+    } else {
+        document.getElementById("scrollToTop").style.display = "none";
+    }
+}
+
+function topFunction() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+const scrollTop = document.getElementById('scrollToTop');
+scrollTop.addEventListener('click', topFunction);
